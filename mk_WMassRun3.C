b6e4bac17ac4fe439609a2b4d9457a5adedd75f2
Emilia <emiliaveikkola@icloud.com>
2026-05-22 09:07:43 +0200

    updated weights, e4, e5, e6

--- mk_WMassRun3.C
+++ mk_WMassRun3.C
@@ -83,7 +83,8 @@ void AddFilesRecursively(const TString &dirPath, TChain *chain) {
       if (file->IsDirectory()) {
          // Always recurse into directories
          AddFilesRecursively(fullPath, chain);
-      } else if (fname.EndsWith(".root") && fullPath.Contains("date-20Mar2026_time-114747_commit-4d9fd29")) { 
+      } else if (fname.EndsWith(".root") &&
+        (fullPath.Contains("2026D"))){ //|| fullPath.Contains("date-10Feb2026"))){//fullPath.Contains("date-23Jan2026_time-104810_commit-4d9fd29")) { 
          //MC: date-14May2025_time-124123_commit-23dc684
          //CDE DataReprocessing: date-08May2025_time-160559_commit-23dc684
          //FGHI Prompt24: date-20May2025_time-131924_commit-23dc684
@@ -99,7 +100,7 @@ void AddFilesRecursively(const TString &dirPath, TChain *chain) {
          //2025G: date-06Nov2025_time-142612_commit-4d9fd29
          //MCWinter25: date-25Nov2025_time-142318_commit-4d9fd29
          //2026A: date-13Mar2026_time-091806_commit-4d9fd29
-         //2026B: date-17Mar2026_time-091009_commit-4d9fd29
+         //2026B: date-24Mar2026_time-112133_commit-65b2668
          std::cout << "Adding file: " << fullPath.Data() << std::endl;
          chain->AddFile(fullPath);
       }
@@ -120,10 +121,11 @@ void mk_WMassRun3() {
    //TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2025/Data/2025E/";
    //TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2025/Data/2025F/";
    //TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2025/Data/2025G/";
+   //TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2025/Data/";
    //TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2025/MCWinter25/";
    //TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2025/Data/";
    //TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2026/Data/2026A";
-   TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2026/Data/2026B";
+   TString mainDirectory = "/eos/user/e/eveikkol/Skim/Wqqm/2026/Data/";
 
    // Recursively add all ROOT files found under the main directory.
    AddFilesRecursively(mainDirectory, c);

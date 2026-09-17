c7bd7a20c49315c4288931b91319aa4973796464
Emilia <emiliaveikkola@icloud.com>
2026-09-17 10:33:27 +0200

    type update

--- CondFormats/JetMETObjects/src/JetResolutionObject.cc
+++ CondFormats/JetMETObjects/src/JetResolutionObject.cc
@@ -57,7 +57,7 @@ namespace JME {
         m_values = std::move(rhs.m_values);
     }
 
-    JetParameters::JetParameters(std::initializer_list<typename value_type::value_type> init) {
+    JetParameters::JetParameters(std::initializer_list<typename JetParameters::value_type::value_type> init) {
         for (auto& i: init) {
             set(i.first, i.second);
         }
